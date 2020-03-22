04f1b2482003c28a2d82fe29d0d4b75e8ed8d4a1
Emil Sundvall <emil@sndvll.com>
2020-03-22 11:23:00 +0100

    added another example entity

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { Server } from './core/Server';
 import Example from './entites/Example';
 import { JsonDbRepository } from './core';
 import {MongoRepository} from './core/repository/MongoRepository';
+import Example2 from './entites/Example2';
+
 
 /**
  * Create a new Server and add entities.
@@ -20,4 +22,5 @@ new Server()
     .applyBodyParser()
     // .addEntity<Example>(Example, new JsonDbRepository<Example>('example'))
     .addEntity<Example>(Example, new MongoRepository<Example>('mongodb://admin:password@localhost:27000', 'example'))
+    .addEntity<Example2>(Example2, new MongoRepository<Example2>('mongodb://admin:password@localhost:27000', 'example'))
     .start();
